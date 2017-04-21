a291433b15f4d55775de31c54f539939a9a85678
Junior <nog.junior84@gmail.com>
2017-04-21 17:56:22 -0300

    Controllers and Models Evento and Video

---
+++ spec/factories/eventos.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :evento do
+    title "MyString"
+    content "MyString"
+  end
+end
---
+++ spec/factories/videos.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :video do
+    title "MyString"
+    video "MyString"
+  end
+end

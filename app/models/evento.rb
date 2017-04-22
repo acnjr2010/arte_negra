86378f87ef7db5d2a2d29825119f2e94f4b2cac0
Junior <nog.junior84@gmail.com>
2017-04-21 21:29:04 -0300

    Models

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -1,2 +1,5 @@
 class Evento < ActiveRecord::Base
+  validates :title, :content, presence: true
+  validates_length_of :title, minimum: 10, maximum: 18
+  validates_length_of :content, minimum: 10, maximum: 130
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,4 @@
 class Video < ActiveRecord::Base
+  validates :title, :video, presence: true
+  validates_length_of :title, minimum: 10, maximum: 43
 end
